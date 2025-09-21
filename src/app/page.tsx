33d91bf4d091e51f93b33b226cac6b1352b55433
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-21 17:51:59 +0400

    feat:import content component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+import { Suspense } from "react";
+import Content from "./components/content/content";
 import Header from "./components/header/header";
 
 
 export default function Home() {
   return (
+    <>
     <Header />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Content />
+    </Suspense>
+    </>
   );
 }
